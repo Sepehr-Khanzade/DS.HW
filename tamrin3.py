b2fba6d42f2eb9fc1a4f086002b6cdffcd799111
Sepehr-Khanzade <151740402+Sepehr-Khanzade@users.noreply.github.com>
2023-12-21 02:08:01 +0330

    Add files via upload

---
+++ tamrin3.py
@@ -0,0 +1,54 @@
+class Queue:
+   
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self.Q = [0] * max_size
+        self.num = 0
+        self.first = 0
+
+    def enqueue(self, item):
+        if self.num >= self.max_size:
+            raise Exception("Queue overflow")
+        self.Q[(self.num + self.first) % self.max_size] = item
+        self.num += 1
+
+    def dequeue(self):
+        if self.num == 0:
+            raise Exception("Queue empty")
+        item = self.Q[self.first]
+        self.first = (self.first + 1) % self.max_size
+        self.num -= 1
+        return item
+
+    def get(self, index):
+        if self.num == 0:
+            raise Exception("Queue empty")
+        if index >= self.num:
+            raise Exception("Index out of range")
+        return self.Q[(self.first + index) % self.max_size]
+
+    def remove(self, index):
+        if self.num == 0:
+            raise Exception("Queue empty")
+        if index >= self.num:
+            raise Exception("Index out of range")
+        if index == 0:
+            return self.dequeue()
+        item = self.Q[(self.first + index) % self.max_size]
+        self.Q[(self.first + index) % self.max_size] = self.Q[(self.first + index - 1) % self.max_size]
+        self.Q[(self.first + index - 1) % self.max_size] = item
+        return item
+
+    def front(self):
+        if self.num == 0:
+            raise Exception("Queue empty")
+        return self.Q[self.first]
+
+    def is_empty(self):
+        return self.num == 0
+
+    def size(self):
+        return self.num
+
+    def is_full(self):
+        return self.num >= self.max_size
